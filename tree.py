5105eff576652af1a065943e5235036aee81370d
naka <naka_syougo@infordio.co.jp>
2017-06-02 11:09:49 +0900

    add function excute

--- tree.py
+++ tree.py
@@ -5,13 +5,13 @@ import random
 class Function(object):
     def __init__(self):
         self.f    = {
-            'x' : 0, # only one input x, can't yet input x, y, ...
-            'c' : 0,
+            'x' : [0, None], # only one input x, can't yet input x, y, ...
+            'c' : [0, None],
         }
         self.keys = None
 
-    def createFunction(self, name, args, check_zero=False):
-        self.f[name] = args
+    def createFunction(self, name, n_arg, f, check_zero=False):
+        self.f[name] = [n_arg, f]
         self.keys    = list(self.f.keys())
 
     def getFunctionDict(self):
@@ -19,15 +19,16 @@ class Function(object):
     
     def getRandomFunction(self, not_arg_zero=False):
         if (not_arg_zero):
-            arg = 0
-            while (arg==0):
+            n_arg = 0
+            while (n_arg==0):
                 f_name = random.choice(self.keys)
-                arg    = self.f[f_name]
+                n_arg  = self.f[f_name][0]
+                f      = self.f[f_name][1]
         else:
             f_name = random.choice(self.keys)
-            arg    = self.f[f_name]
-        
-        return (f_name, arg)
+            n_arg  = self.f[f_name][0]
+            f      = self.f[f_name][1]
+        return (f_name, n_arg, f)
 
     def show(self):
         print("f:    ", self.f)
@@ -35,9 +36,10 @@ class Function(object):
 
 class Node(object):
     def __init__(self):
-        self.node_id   = ""
+        self.node_id  = ""
         self.f_name   = None
-        self.arg      = None
+        self.n_arg    = None
+        self.f        = None
         self.parent   = None
         self.children = []
 
@@ -47,12 +49,13 @@ class Node(object):
     def getNodeId(self):
         return (self.node_id)
         
-    def setFunction(self, f_name, arg):
+    def setFunction(self, f_name, n_arg, f):
         self.f_name = f_name
-        self.arg    = arg
+        self.n_arg  = n_arg
+        self.f      = f
 
     def getFunction(self):
-        return (self.f_name, self.arg)
+        return (self.f_name, self.n_arg, self.f)
 
     def setParent(self, parent):
         self.parent = parent
@@ -66,11 +69,15 @@ class Node(object):
     def getChildren(self):
         return (self.children)
 
+    def excute(self, args):
+        return (self.f(args))
+
     def show(self):
         print("node:     ", self)
         print("node_id:  ", self.node_id)
         print("f_name:   ", self.f_name)
-        print("arg:      ", self.arg)
+        print("n_arg:    ", self.n_arg)
+        print("f:        ", self.f)
         print("parent:   ", self.parent)
         print("children: ", self.children)
 
@@ -95,22 +102,22 @@ class Tree(object):
         node_list        = self.getNodeList()
         return_node_list = []
         for node in node_list:
-            node_f_name, _ = node.getFunction()
+            node_f_name, _, _ = node.getFunction()
             if (node_f_name==f_name):
                 return_node_list.append(node)
         return (return_node_list)
 
     def build(self, inputs, Function, width=None, depth=None, number_of_node=10):
         # set root
-        f_name, arg = Function.getRandomFunction()
-        root        = Node()
+        f_name, n_arg, f = Function.getRandomFunction()
+        root             = Node()
         root.setNodeId("0")
-        root.setFunction(f_name, arg)
+        root.setFunction(f_name, n_arg, f)
         self.setRoot(root)
         self.addNodeList(root)
 
         # build tree
-        if (arg==0):
+        if (n_arg==0):
             DONE  = True
         else:
             DONE  = False
@@ -119,14 +126,14 @@ class Tree(object):
         node_list = self.getNodeList()
         i         = 0
         while (not(DONE)):
-            cur_node = node_list[i]
-            children = cur_node.getChildren()
-            _, arg   = cur_node.getFunction()
-            if (len(children)<arg):
+            cur_node     = node_list[i]
+            children     = cur_node.getChildren()
+            _, n_arg, _  = cur_node.getFunction()
+            if (len(children)<n_arg):
                 new_node = Node()
-                f_name, arg = Function.getRandomFunction(not_arg_zero=True)
+                f_name, n_arg, f = Function.getRandomFunction(not_arg_zero=True)
                 new_node.setNodeId("{0}{1}".format(cur_node.getNodeId(), len(children)))
-                new_node.setFunction(f_name, arg)
+                new_node.setFunction(f_name, n_arg, f)
                 new_node.setParent(cur_node)
                 cur_node.addChildren(new_node)
                 self.addNodeList(new_node)
@@ -134,6 +141,7 @@ class Tree(object):
                 i += 1
                 if (i==number_of_node):
                     DONE = True
+        # set 'x' or 'c' to build tree
         node_list = self.getNodeList()
         for node in node_list:
             children = node.getChildren()
@@ -141,9 +149,9 @@ class Tree(object):
                 r = random.randint(0, 1)
                 l = self.getNodeListByFunction('x')
                 if (r==0 or len(l)<len(inputs)):
-                    node.setFunction('x', 0)
+                    node.setFunction('x', 0, None)
                 else:
-                    node.setFunction('c', 0)
+                    node.setFunction('c', 0, None)
 
     def show(self):
         print("root: "     , self.root)
@@ -153,24 +161,14 @@ class Tree(object):
             node.show()
             print("---------------")
 
-        """
-        DONE     = False
-        i        = 0
-        while (not(DONE)):
-            cur_node = self.node_list[i]
-            
-            if (cur_node):
-        """
-                
-
 if __name__ == "__main__":
     tree = Tree()
     tree.show()
     function = Function()
-    function.createFunction('+', 2)
-    function.createFunction('-', 2)
-    function.createFunction('*', 2)
-    function.createFunction('/', 2)
+    function.createFunction('+', 2, lambda a:a[0]+a[1])
+    function.createFunction('-', 2, lambda a:a[0]-a[1])
+    function.createFunction('*', 2, lambda a:a[0]*a[1])
+    function.createFunction('/', 2, lambda a:a[0]/a[1] if a[1]!=0 else 0)
     function.show()
     
     inputs    = [i for i in range(1)]
@@ -178,5 +176,3 @@ if __name__ == "__main__":
     node_list = tree.getNodeList()
     x_nodes   = tree.getNodeListByFunction('x')
     tree.show()
-    #print("node_list: ", node_list, len(node_list))
-    #print("x_nodes:   ", x_nodes,   len(x_nodes))
